54de3043c1e0159e98315a1c5ba729a5634d2411
Tran Quang Trung (PPM) <trungtq@bagroup.vn>
2020-12-04 15:37:36 +0700

    note tạm

--- Core/ExcelExportNoTemplate.cs
+++ Core/ExcelExportNoTemplate.cs
@@ -1,7 +1,9 @@
 ﻿using NPOI.SS.UserModel;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace BAExcelExport
@@ -51,7 +53,49 @@ namespace BAExcelExport
 
         protected override void RenderBody()
         {
+            PropertyInfo[] propertyInfos = typeof(TEntity).GetProperties();
 
+            IRow sheetRow = null;
+
+            ICellStyle CellCentertTopAlignment = this.Workbook.CreateCellStyle();
+            CellCentertTopAlignment.Alignment = HorizontalAlignment.Center;
+            CellCentertTopAlignment.VerticalAlignment = VerticalAlignment.Center;
+
+            string formatPart = string.Empty;
+
+            for (int i = 0; i < this.DataSource.Count; i++)
+            {
+                sheetRow = this.Sheet.CreateRow(i + 1);
+
+                for (int j = 0; j < propertyInfos.Length; j++)
+                {
+
+                    ICell cellRow = sheetRow.CreateCell(j);
+
+                    object cellvalue = propertyInfos[j].GetValue(this.DataSource[i], null);
+
+                    if (cellvalue != null)
+                    {
+
+                        // Kiểm tra giá trị có là số không?
+                        if (cellvalue.IsNumeric())
+                        {
+                            cellRow.SetCellValue(string.Format(new CultureInfo("en-US"), "{0:" + formatPart + "}", cellvalue));
+                        }
+                        else
+                        {
+                            cellRow.SetCellValue(string.Format("{0:" + formatPart + "}", cellvalue));
+                        }
+                    }
+                    else
+                    {
+                        cellRow.SetCellValue(string.Empty);
+                    }
+
+                    cellRow.CellStyle = CellCentertTopAlignment;
+                    cellRow.CellStyle.WrapText = true;
+                }
+            }
         }
     }
 }
