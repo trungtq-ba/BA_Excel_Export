09849a0003985acc3913be66fb4ea8c890f07c69
Tran Quang Trung (PPM) <trungtq@bagroup.vn>
2020-12-10 14:48:08 +0700

    1 bản chạy với FluentExcel

--- Controllers/FluentExcelController.cs
+++ Controllers/FluentExcelController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BAExcelExport.Extensions;
+using BAExcelExport.Models;
+using FluentExcel;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace BAExcelExport.Controllers
@@ -13,35 +19,122 @@ namespace BAExcelExport.Controllers
     {
         private const int MAX_RECORD = 100;
 
+
         // GET api/Excel/excelExport
         [HttpGet]
         [Route("fluentExcelExport")]
-        public ActionResult ExcelExport()
+        public ActionResult FluentExcelExport()
         {
-            var dataInput = DataHelper.GenerateData(MAX_RECORD);
-
-
-            ReportSourceTemplate<ReportDataModel> template = new ReportSourceTemplate<ReportDataModel>()
-            {
-                ReportTitle = "BÁO CÁO DANH SÁCH ĐIỂM",
-                ReportSubtitleLevel1 = "TIÊU ĐỀ CON CỦA BÁO CÁO DANH SÁCH ĐIỂM",
-                ReportSubtitleLevel2 = "MÔ TẢ CỦA BÁO CÁO DANH SÁCH ĐIỂM",
-                FileName = typeof(ReportDataModel).Name,
-                SettingColumns = null,
-                ReportList = dataInput
-            };
-
-
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            var resultContent = ReportHelper.GenerateReport(template);
+            // global call this
+            FluentConfiguration();
 
-            string fileName = ReportHelper.GetFileName(template.FileName);
+            // demo the extension point
+            Excel.Setting.For<Report>().FromAnnotations()
+                                       .AdjustAutoIndex();
+
+            // Change title cell style
+            //Excel.Setting.TitleCellStyleApplier = MyTitleCellApplier;
+
+            var reports = new Report[MAX_RECORD];
+            for (int i = 0; i < MAX_RECORD; i++)
+            {
+                reports[i] = new Report
+                {
+                    City = "ningbo",
+                    Building = "世茂首府",
+                    HandleTime = DateTime.Now.AddDays(7 * i),
+                    Broker = "rigofunc 18957139**7",
+                    Customer = "yingting 18957139**7",
+                    Room = "103",
+                    Brokerage = 125 * i,
+                    Profits = 25 * i
+                };
+            }
+
+            // save to excel file with multiple sheets based on expression
+            var data = reports.ToExcelContent("BAGPS", int.MaxValue, true);
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(data)
+            };
+
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+            string fileName = ReportHelper.GetFileName(typeof(Report).Name);
 
             sw.Stop();
 
-            return File(resultContent.Content.ReadAsByteArrayAsync().Result, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sw.ElapsedMilliseconds + "_" + fileName);
+            return File(response.Content.ReadAsByteArrayAsync().Result, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sw.ElapsedMilliseconds + "_" + fileName);
+
+        }
+
+        /// <summary>
+        /// Use fluent configuration api. (doesn't poison your POCO)
+        /// </summary>
+        private static void FluentConfiguration()
+        {
+            var fc = Excel.Setting.For<Report>();
+
+            fc.HasStatistics("合计", "SUM", 6, 7)
+              .HasFilter(firstColumn: 0, lastColumn: 2, firstRow: 0)
+              .HasFreeze(columnSplit: 2, rowSplit: 1, leftMostColumn: 2, topMostRow: 1);
+
+            fc.Property(r => r.City)
+              .HasExcelIndex(0)
+              .HasExcelTitle("城市")
+              .IsMergeEnabled();
+
+            // or
+            //fc.Property(r => r.City).HasExcelCell(0,"城市", allowMerge: true);
+
+            fc.Property(r => r.Building)
+              .HasExcelIndex(1)
+              .HasExcelTitle("楼盘")
+              .IsMergeEnabled();
+
+            // configures the ignore when exporting or importing.
+            fc.Property(r => r.Area)
+              .HasExcelIndex(8)
+              .HasExcelTitle("Area")
+              .IsIgnored(exportingIsIgnored: false, importingIsIgnored: true);
+
+            // or
+            //fc.Property(r => r.Area).IsIgnored(8, "Area", formatter: null, exportingIsIgnored: false, importingIsIgnored: true);
+
+            fc.Property(r => r.HandleTime)
+              .HasExcelIndex(2)
+              .HasExcelTitle("成交时间")
+              .HasDataFormatter("yyyy-MM-dd");
+
+            // or
+            //fc.Property(r => r.HandleTime).HasExcelCell(2, "成交时间", formatter: "yyyy-MM-dd", allowMerge: false);
+            // or
+            //fc.Property(r => r.HandleTime).HasExcelCell(2, "成交时间", "yyyy-MM-dd");
+
+            fc.Property(r => r.Broker)
+              .HasExcelIndex(3)
+              .HasExcelTitle("经纪人");
+
+            fc.Property(r => r.Customer)
+              .HasExcelIndex(4)
+              .HasExcelTitle("客户");
+
+            fc.Property(r => r.Room)
+              .HasExcelIndex(5)
+              .HasExcelTitle("房源");
+
+            fc.Property(r => r.Brokerage)
+              .HasExcelIndex(6)
+              .HasDataFormatter("￥0.00")
+              .HasExcelTitle("佣金(元)");
+
+            fc.Property(r => r.Profits)
+              .HasExcelIndex(7)
+              .HasExcelTitle("收益(元)");
         }
     }
 }
---
+++ Models/Report.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BAExcelExport.Models
+{
+    public class Report
+    {
+        [Display(Name = "Thành phố")]
+        public string City { get; set; }
+        [Display(Name = "Tòa nhà")]
+        public string Building { get; set; }
+        [Display(Name = "Khu vực")]
+        public string Area { get; set; }
+        [Display(Name = "Thòi gian mở cửa")]
+        public DateTime HandleTime { get; set; }
+        [Display(Name = "Nhà môi giới")]
+        public string Broker { get; set; }
+        [Display(Name = "Khách hàng")]
+        public string Customer { get; set; }
+        [Display(Name = "Phòng")]
+        public string Room { get; set; }
+        [Display(Name = "Môi giới")]
+        public decimal Brokerage { get; set; }
+        [Display(Name = "Lợi nhuận")]
+        public decimal Profits { get; set; }
+    }
+}
